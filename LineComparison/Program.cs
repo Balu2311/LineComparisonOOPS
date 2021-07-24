68dac23af61eb6c1a890114f967a0abeb8023aff
Vallem Balu <vbnreddy5@gmail.com>
2021-07-24 15:20:35 +0530

    Calculate Length of Line

---
+++ LineComparison/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************Welcome To Line Comparison*******");
+            //UC1_Length_of_lines.Lenth_of_points();
+            UC2_Equality_of_lines.equalOrNot();
+            //UC3_CompareToLines.Equal_to_lines();
+
+
+
+        }
+    }
+}
