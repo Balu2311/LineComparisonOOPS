b5e9fb06ddfaf3f998693af75a28c2181dfaff19
Vallem Balu <vbnreddy5@gmail.com>
2021-07-24 15:52:08 +0530

    Comapere the two Lines

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -8,8 +8,8 @@ namespace LineComparison
         {
             Console.WriteLine("************Welcome To Line Comparison*******");
             //UC1_Length_of_lines.Lenth_of_points();
-            UC2_Equality_of_lines.equalOrNot();
-            //UC3_CompareToLines.Equal_to_lines();
+            //UC2_Equality_of_lines.equalOrNot();
+            UC3_CompareToLines.Equal_to_lines();
 
 
 
